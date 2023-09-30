f035208ecff555db55a17a4a3e912fae54abeb98
HuayuanSong <huayuan.s@gmail.com>
2023-10-01 01:20:57 +0200

    Done with RDS for now

--- RDS.py
+++ RDS.py
@@ -3,6 +3,12 @@ import random
 
 class DigitSpanTest:
     def __init__(self, master):
+        """
+        Initialize the DigitSpanTest application.
+
+        Args:
+            master (tk.Tk): The master Tkinter window.
+        """
         self.master = master
         self.master.title("Digit Span Test for Working Memory Evaluation")
 
@@ -16,6 +22,9 @@ class DigitSpanTest:
         self.ask_for_id()
 
     def ask_for_id(self):
+        """
+        Create a screen to ask for the user's ID.
+        """
         self.canvas.delete('all')
         label = self.canvas.create_text(self.width/2, self.height/2.3, fill='darkblue', font='Arial 26', text="Please enter your ID:", justify='c')
         self.id_entry = tk.Entry(self.master, font=('Arial', 32))
@@ -23,11 +32,17 @@ class DigitSpanTest:
         self.id_entry.bind('<Return>', self.start_intro)
 
     def start_intro(self, event=None):
+        """
+        Start the introduction phase of the test.
+
+        Args:
+            event: Event object (default is None).
+        """
         self.user_id = self.id_entry.get().strip()
         if not self.user_id:
             return  # Don't proceed without ID
+
         self.canvas.delete('all')
-        
         title_text = "Digit Span Test for Working Memory Evaluation"
         self.canvas.create_text(self.width/2, self.height/4.5, fill='darkblue', font='Arial 52', text=title_text, justify='c')
 
@@ -49,47 +64,66 @@ class DigitSpanTest:
         self.score = 0
 
     def start_test(self):
+        """
+        Start the Digit Span Test.
+        """
         self.start_btn.destroy()
         self.run_test()
 
     def run_test(self):
-        self.canvas.delete('all')
+        """
+        Run the test, showing sequences and collecting user input.
+        """
         if self.sequence_attempts < 2:
-            self.sequence = [str(random.randint(0, 9)) for _ in range(self.sequence_length)]
+            # Generate a random sequence of digits with the specified length.
+            while True:
+                self.sequence = [str(random.randint(0, 9)) for _ in range(self.sequence_length)]
+                if self.consecutive(self.sequence):
+                    break
+            # After a delay, show the sequence to the user.
             self.master.after(500, self.show_sequence)
         else:
             if self.correct_attempts >= 1:
+                # If the user got the sequence correct, increase length and restart.
                 self.correct_attempts = 0
                 self.sequence_attempts = 0
                 self.sequence_length += 1
                 self.run_test()
             else:
                 if self.forward:
+                    # If the user fails to repeat forwards, switch to backwards.
                     self.forward = False
                     self.sequence_length = 2
                     self.sequence_attempts = 0
                     self.correct_attempts = 0
-                    self.canvas.create_text(self.width/2, self.height/2.3, fill='darkblue', font='Arial 26', text="Now, input the numbers backwards.", justify='c')
-                    self.run_test()
+                    self.show_backwards_notice()
                 else:
+                    # If the user fails both forwards and backwards, end the test.
                     self.end_test()
 
     def show_sequence(self):
-        self.canvas.delete('all')
+        """
+        Display the sequence of digits to the user.
+        """
         if not hasattr(self, "sequence_index"):
             self.sequence_index = 0
+
+        self.canvas.delete('all')
 
         if self.sequence_index < len(self.sequence):
             num = self.sequence[self.sequence_index]
             self.canvas.create_text(self.width/2, self.height/2, fill='darkblue', font='Times 160', text=num, justify='c')
             self.sequence_index += 1
+            # After a delay, show the next digit in the sequence.
             self.master.after(1000, self.show_sequence)
         else:
             self.sequence_index = 0
             self.get_input()
 
     def get_input(self):
-        self.canvas.delete('all')
+        """
+        Display an input field for the user to enter the sequence they saw.
+        """
         prompt = "Input the numbers forwards:" if self.forward else "Input the numbers backwards:"
         self.canvas.create_text(self.width/2, self.height/2.3, fill='darkblue', font='Arial 26', text=prompt, justify='c')
         
@@ -98,10 +132,15 @@ class DigitSpanTest:
         self.input_entry.bind('<Return>', self.validate_input)
 
     def validate_input(self, event):
+        """
+        Validate the user's input and continue the test.
+
+        Args:
+            event: Event object.
+        """
         user_input = self.input_entry.get()
         self.input_entry.destroy()
-        self.canvas.delete('all')
-        
+
         correct_sequence = ''.join(self.sequence)
         if not self.forward:
             correct_sequence = correct_sequence[::-1]
@@ -114,14 +153,41 @@ class DigitSpanTest:
         self.run_test()
 
     def end_test(self):
+        """
+        End the test and display the user's score.
+        """
         self.canvas.delete('all')
         self.canvas.create_text(self.width/2, self.height/2.3, fill='darkblue', font='Arial 26', text="Thank you for your participation!", justify='c')
         self.canvas.create_text(self.width/2, self.height/2, fill='darkblue', font='Arial 36', text=f"Your score: {self.score}", justify='c')
 
-        # Save results
+        # Save results to a file.
         with open(f"data/{self.user_id}_results.txt", 'w') as f:
             f.write(f"User ID: {self.user_id}\n")
             f.write(f"Score: {self.score}\n")
+
+    def consecutive(self, sequence):
+        """
+        Check if a sequence of digits is consecutive.
+
+        Args:
+            sequence (list): List of digits.
+
+        Returns:
+            bool: True if the sequence is consecutive, False otherwise.
+        """
+        l = len(sequence)
+        for x in range(l-1):
+            if sequence[x] == sequence[x+1] or abs(int(sequence[x]) - int(sequence[x+1])) == 1:
+                return False
+        return True
+
+    def show_backwards_notice(self):
+        """
+        Show a notice for the user to input numbers backwards.
+        """
+        self.canvas.delete('all')
+        self.canvas.create_text(self.width/2, self.height/2.3, fill='darkblue', font='Arial 26', text="Now, input the numbers backwards.", justify='c')
+        self.master.after(3000, self.run_test)
 
 if __name__ == "__main__":
     root = tk.Tk()
