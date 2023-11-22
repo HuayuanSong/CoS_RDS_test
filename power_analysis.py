65b3df76337cc21b047f191e64b9c6b635ea1ca9
Marina Epitropakis <>
2023-11-22 11:40:29 +0100

    added more analysis

--- power_analysis.py
+++ power_analysis.py
@@ -22,6 +22,16 @@ def calculate_cohens_d(group1, group2):
 
     return cohen_d
 
+def calculate_effect_size(sample_size, alpha, beta):
+    # Calculate critical values
+    z_alpha_over_2 = norm.ppf(1 - alpha/2)
+    z_beta = norm.ppf(1 - beta)
+
+    # Calculate effect size
+    effect_size = (2 * (z_alpha_over_2 + z_beta)) / (sample_size**0.5)
+
+    return effect_size
+
 def calculate_sample_size_ttest(effect_size, alpha, power):
     analysis = TTestIndPower()
     sample_size = analysis.solve_power(effect_size=effect_size, alpha=alpha, power=power, ratio=1.0, alternative='two-sided')
@@ -60,10 +70,9 @@ print("TikTok Group:", tiktok_group)
 # Calculates Cohen's d
 effect_size = calculate_cohens_d(video_group, tiktok_group)
 
-# Prints the result
-print(f"Cohen's d: {effect_size}")
-
 # Sets alpha and desired power
+sample_size_lit = 106
+simple_size_our_data = 25
 alpha = 0.05
 power = 0.8
 beta = 0.2
@@ -71,14 +80,21 @@ beta = 0.2
 # Calculates required sample size
 sample_size_builtin = calculate_sample_size_ttest(effect_size, alpha, power)
 
-# Prints the result
-print(f"Builtin ttest function: Required sample size per group: {sample_size_builtin}")
 
 # Calculate required sample size
-sample_size_manual, z_alpha_over_2, z_beta  = calculate_sample_size_manual(alpha, beta, pooled_sd, effect_size)
+sample_size_manual, z_alpha_over_2, z_beta  = calculate_sample_size_manual(alpha, beta, effect_size)
+
+# Calculate effect size
+calculated_effect_size = calculate_effect_size(sample_size_lit, alpha, beta)
+calculated_effect_size_our_sample = calculate_effect_size(simple_size_our_data, alpha, beta)
 
 # Print the result
+print(f"Cohen's d from our analysis: {effect_size}")
+print(f"Builtin ttest function: Required sample size per group: {sample_size_builtin}")
 print(f"Manual function: Required sample size per group: {sample_size_manual}")
+print(f"Calculated Effect Size from similar literature: {calculated_effect_size}")
+print(f"Effect Size from our sample size: {calculated_effect_size_our_sample}")
+
 print(f"Zalpha: {z_alpha_over_2}")
 print(f"z_beta: {z_beta}")
 
